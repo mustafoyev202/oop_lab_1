75dcb086d811ae1c270d14dfa3726e5d5f4e28aa
mustafoyev202 <b.mustafoyev@newuu.uz>
2025-08-26 14:28:05 +0500

    Initial Commit

---
+++ oop_lab_1/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello, World!\n";
+    for (int i=0; i<5;i++){
+        std::cout<<i<<" ";
+    }
+    return 0;
+}
